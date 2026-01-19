e1f50e7c64351cd5cf40b88e2bb34e7cc6edaba2
lm379 <2645081603@qq.com>
2026-01-20 00:54:24 +0800

    build: 添加 AWS SDK 作为外部服务器依赖包

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/s3-request-presigner'],
 };
- 
+
 export default withNextIntl(nextConfig);
